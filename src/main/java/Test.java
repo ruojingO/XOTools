9a81b98e5eec79dd6cc26758ef5ebda4bc579dc8
RUOJING <magic0199@163.com>
2025-05-26 16:13:40 +0800

    tt

---
+++ src/main/java/Test.java
@@ -0,0 +1,11 @@
+
+import java.text.SimpleDateFormat;
+
+public class Test {
+    private static SimpleDateFormat sd = new SimpleDateFormat("yyyy-MM-dd");
+
+    public static void main(String[] args) {
+        System.out.println(1 / 0); // Valid operation
+
+    }
+}
